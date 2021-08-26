379eb757bb9387cfa18f6e91f700c6214a298ade
AtaKaboudi <ata.kaboudi@gmail.com>
2021-08-26 19:41:55 +0100

    PNG Logo

--- frontend/src/components/form/Signup.jsx
+++ frontend/src/components/form/Signup.jsx
@@ -1,16 +1,28 @@
 import "./signup.scss";
-
+import { useState } from "react";
+import logoPng from '../../cure.png'
 export default function Signup() {
+
+let [holderEmail,setHolderEmail] = useState('Email')
+let [holderPassword,setholderPassword] = useState('Password')
+let [holderId,setHolderId] = useState('CureId')
+
   return (
     <div className="signup">
+      <img alt = "logo" src={logoPng} />
       <h1>digibionics</h1>
       <h2>Partner</h2>
         <form>
           <h1>Sign In</h1>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="CureID" />
-          <button> Login </button>
+          <input type="text" placeholder={holderEmail} onClick={() => setHolderEmail('')} onChange={(e) => {setHolderEmail(e.target.textContent)}}/>
+          <input type="password" placeholder={holderPassword} placeholder={holderPassword} onClick={() => setholderPassword('')} onChange={(e) => { setholderPassword(e.target.textContent)}} />
+          <input type="id" placeholder={holderId} placeholder={holderId} onClick={() => setHolderId('')} onChange={(e) => { setHolderId(e.target.textContent)}} />
+        <button>
+          Login
+        <span className="material-icons">
+        chevron_right
+        </span>
+        </button>
         </form>
     </div>
   );
